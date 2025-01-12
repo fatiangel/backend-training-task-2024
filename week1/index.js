611ed25d35f1742e996adbc51106467da5ad4e10
fatiangel <fireicelord@hotmail.com>
2025-01-12 21:47:15 +0800

    task1

--- week1/index.js
+++ week1/index.js
@@ -6,8 +6,15 @@
 // 3. 請宣告一個 `const` 變數名稱為 `isRunningOnTreadmill`，並賦予值為 `true`。
 // 4. 使用 `console.log()` 輸出 `alexAge`、`alexMembershipID` 和 `isRunningOnTreadmill`。
 
-// 1.1 範例
+// 1.1
 const alexAge = 25;
+// 1.2
+const alexMembershipID = "GYM2024-12345";
+// 1.3
+const isRunningOnTreadmill = true;
+// 1.4 print variables to console
+console.log(alexAge, alexMembershipID, isRunningOnTreadmill);
+
 
 // ### 題目二：變數命名練習
 // - 瑜伽團課 - 300 元
@@ -16,28 +23,62 @@ const alexAge = 25;
 // 情境：Alex 這個月的運動預算有 3000 元
 // 請修改以下中文變數名稱，讓他符合變數語意
 
-const 變數一 = 300;
-const 變數二 = 500;
-const 變數三 = 1500;
-const 變數四 = 3000;
+// const 變數一 = 300;
+// const 變數二 = 500;
+// const 變數三 = 1500;
+// const 變數四 = 3000;
+const yogaGroupClass = 300; // 瑜伽團課
+const weightTrainingGroupClass = 500; // 重訓團課
+const weightTraining1on1 = 1500; // 重訓 1 對 1 課程
+const monthlyBudget = 3000; // Alex 這個月的運動預算
+
 
 // ### 題目三：變數計算
 // 呈上題，Alex 想要規劃好自己的運動課程，需符合以下三個條件，請將花費總數計算在 AlexBudget 上，一起幫幫他吧！
 // 條件一：一定至少要買 1 堂重訓團課和瑜伽團課
 // 條件二：瑜伽團課只能一次買 3 堂
 // 條件三：一定要花到 2400 以上，並購買 5 堂課程
+let yogaClassSum = 6; // 瑜伽團課
+let weightClassSum = 2; // 重訓團課
+// yogaClassSum > 0 && weightClassSum > 0 ;
+let totalClassSum = yogaClassSum + weightClassSum;
+let totalClassCost = yogaGroupClass * yogaClassSum + weightTrainingGroupClass * weightClassSum;
+const AlexBudget = monthlyBudget - totalClassCost;
+if(totalClassCost >= 2400 && totalClassCost <=3000){
+  if(totalClassSum > 5){
+    console.log("達成條件")
+  }else{
+    console.log("未達成條件")
+  }
+}else{
+  console.log("未達成條件")
+}
+//3.1
+console.log(`Alex購買課程後，共剩下 ${AlexBudget}元`);
 
-console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`);
 
 // ### 題目四：線稿圖截圖，看圖宣告變數
 // 請參考資料夾內 q4.webp 圖片
 // 請依照你看到的內容來嘗試設計變數和值（至少 3 個）
+// classCourseBundlesOriginalPrice = 2800;
+const classCourseBundlesOriginalPrice = 2800;
+const classCourseBundlesPrice = classCourseBundlesOriginalPrice * 0.9;
+const classCourseBundlesSum = 14;
+const classCourseBundlesTime = 50;
+const classCourseBundlesAverage = classCourseBundlesPrice / classCourseBundlesSum;
+
 
 // ### 題目五：布林值與變數定義，看是否有用對 const, let
 // 情境：Alex 在往健身房的路上，望向城市的風景，請描述她看到的一切，並宣告變數與賦予值
 // 4-1. Alex 在等紅綠燈，他抬頭看一下現在是紅燈，還有 28 秒綠燈（最多 3 個宣告）
 // 4-2. 目前一起等待的機車有 8 台
 // 4.3. Alex 望向天空，看到天上有 5 朵白雲和 1 顆太陽
+let trafficLightIsRed = true;
+let trafficLightIsGreen = 28;
+let waittingScooter = 8;
+let clouds = 5;
+const sun = 1;
+
 
 // ### 題目六：情境題：簡單變數計算
 // 情境：Alex 每天都會帶著 2000cc 的水壺
@@ -50,8 +91,12 @@ console.log(`Alex 買完課程了，他一共剩下 ${AlexBudget} 元`);
 
 let myWater = 2000; // 水壺容量
 myWater -= 500; // 早上喝了 500cc
+myWater -= 800;// 中午又喝了 800cc
+myWater += 1000;// 下午去健身前，先裝了 1000cc 的水
+myWater -= 700;// 健身時，又喝掉了 700cc
 
 console.log(`Alex 的水壺還有 ${myWater}cc 的水`);
+
 
 // ### 題目七：情境題：變數計算
 // 情境：Anna 每週都會到單次計費型的健身房運動，週日運動結束後，想知道自己本週的消費金額，但結帳系統出了點問題，Anna 決定自己用 JS 來計算。
@@ -62,6 +107,12 @@ console.log(`Alex 的水壺還有 ${myWater}cc 的水`);
 
 let totalBill = 0;
 const machineUsePrice = 50;
+const machineGCPrice = 150;
+let thisWeekHours = 3;
+let thisWeekGroupClasses = 2;
+let machineUsePriceTotal = machineUsePrice * thisWeekHours;
+let groupClassesTotal = machineGCPrice * thisWeekGroupClasses;
+let total = totalBill + machineUsePriceTotal + groupClassesTotal;
 
 console.log(
   `Anna 本週器械使用費共 ${machineUsePriceTotal} 元，團體課費用共 ${groupClassesTotal} 元，一共消費金額是 ${total}元`
@@ -71,11 +122,11 @@ console.log(
 // 情境：請依照以下程式碼告知答案是多少，並在下方用註解方式寫上這五行程式碼做了什麼事
 // 以下程式碼請勿變更
 let a = 8; // 範例：宣告了一個 a 的變數，並賦予了一個 8 的數字型別
-let b = 0;
-a = 13;
-a = b + 4;
-a - b;
-b += 1;
+let b = 0; // 宣告了一個 b 的變數，並賦予了一個 0 的數字型別
+a = 13;    // 將 a 的值重新賦予為 13
+a = b + 4; // 將 a 的值重新賦予為 b + 4
+a - b;     // 將 a 減去 b 的值，但沒有重新賦予給任何變數
+b += 1;    // 將 b 的值加 1，並重新賦予給 b
 
 // ### 題目九：型別查詢
 // 請不要觀看 console.log，透過註解告知解答每個變數的型別
@@ -85,10 +136,16 @@ let e = c + d;
 let f = false;
 let g = d + d;
 let h = f + g;
-
 // 請從以下新增註解，告知上面每行各別是哪些型別
-// a 是 string
-// b 是 ???
+// a 是 number
+// b 是 number
+// c 是 string
+// d 是 number
+// e 是 string
+// e 是 string
+// f 為boolean
+// g 為number
+// h 為number
 
 // ### 題目十：傳值與傳參考
 // 情境：請依照程式碼告知答案是多少，並在下方用註解方式寫上這五行程式碼做了什麼事
@@ -99,3 +156,11 @@ let numberArr2 = numberArr1;
 numberArr2.push(20);
 numberArr2 = [25, 30, 35];
 console.log(numberArr1, numberArr2);
+
+// 下方用註解方式寫上這五行程式碼做了什麼事
+// 宣告 numberArr1 為 [5, 10, 15]
+// 宣告 numberArr2 為 numberArr1的值, 此為傳址
+// numberArr2 在原本記憶體的陣列後面加入 20, 為 [5, 10, 15, 20]
+// numberArr2 重新指向一個新的陣列, 且不再指向原本的陣列, 及numberArr1 保持原本的陣列
+// numberArr2 指向新的陣列 [25, 30, 35]
+// console.log(numberArr1, numberArr2) 為 [5, 10, 15, 20] [25, 30, 35]
